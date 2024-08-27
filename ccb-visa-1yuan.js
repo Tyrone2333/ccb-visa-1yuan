66d4c4074a23e00d4b0c2f133c20e05576b00cc1
en20 <a424021424@gmail.com>
2024-08-27 10:03:05 +0800

    Update ccb-visa-1yuan.js

--- ccb-visa-1yuan.js
+++ ccb-visa-1yuan.js
@@ -34,6 +34,7 @@ async function checkGoodsList() {
     }, {
       headers: {
         'Content-Type': 'application/json;charset=UTF-8',
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1',
       },
     })
 
